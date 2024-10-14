6a03d0581da56e6680ead6856e9f7969c4caebdb
hakoom911 <114762298+hakoom911@users.noreply.github.com>
2024-10-15 00:11:40 +0300

    - adding the animation of blink to tailwind config file. - add the pop function to the array if the last element in it is " " in insertion function. - update moveCursorRight function condition and the cursor set function values. - update insert newLine function. - adding new function of backSpaceDelete function. - update the code of deleteCharacter function. - and more...

--- src/components/TextEditor.tsx
+++ src/components/TextEditor.tsx
@@ -9,23 +9,27 @@ import { useState } from "react";
 type Props = {};
 
 export default function TextEditor({}: Props) {
-  const [text, setText] = useState([[""]]);
-  const [cursor, setCursor] = useState({ row: 0, col: 2 });
+  const [text, setText] = useState<string[][]>([[" "]]);
+  const [cursor, setCursor] = useState({ row: 0, col: 0 });
 
-  const insertCharacter = (char: string) => {
+  const insertCharacter = (char: string, type?: "space") => {
     const newText = [...text];
+    const newChar = type === "space" ? "space" : char;
+    if (newText[cursor.row][newText[cursor.row].length - 1] === " ") {
+      newText[cursor.row].pop();
+    }
     newText[cursor.row] = [
       ...newText[cursor.row].slice(0, cursor.col),
-      char, 
+      newChar,
       ...newText[cursor.row].slice(cursor.col),
     ];
-  
+    newText[cursor.row].push(" ");
     setText(newText);
     setCursor({ row: cursor.row, col: cursor.col + 1 });
   };
 
   const moveCursorRight = () => {
-    if (cursor.col < text[cursor.row].length) {
+    if (cursor.col < text[cursor.row].length - 1) {
       setCursor({ ...cursor, col: cursor.col + 1 });
     } else if (cursor.row < text.length - 1) {
       setCursor({ row: cursor.row + 1, col: 0 });
@@ -36,7 +40,7 @@ export default function TextEditor({}: Props) {
     if (cursor.col > 0) {
       setCursor({ ...cursor, col: cursor.col - 1 });
     } else if (cursor.row > 0) {
-      setCursor({ row: cursor.row - 1, col: text[cursor.row - 1].length });
+      setCursor({ row: cursor.row - 1, col: text[cursor.row - 1].length - 1 });
     }
   };
 
@@ -46,12 +50,13 @@ export default function TextEditor({}: Props) {
     const newLine = currentLine.slice(cursor.col);
     newText[cursor.row] = currentLine.slice(0, cursor.col);
     newText.splice(cursor.row + 1, 0, newLine);
+    newText[cursor.row].push(" ");
 
     setText(newText);
     setCursor({ row: cursor.row + 1, col: 0 });
   };
 
-  const deleteCharacter = () => {
+  const backSpaceDeleteCharacter = () => {
     if (cursor.col > 0) {
       const newText = [...text];
       const currentLine = newText[cursor.row];
@@ -66,7 +71,7 @@ export default function TextEditor({}: Props) {
     } else if (cursor.row > 0) {
       const newText = [...text];
       const previousLine = newText[cursor.row - 1];
-
+      previousLine.pop();
       newText[cursor.row - 1] = [...previousLine, ...newText[cursor.row]];
       newText.splice(cursor.row, 1);
 
@@ -75,9 +80,36 @@ export default function TextEditor({}: Props) {
     }
   };
 
-  const handleKeyDown = (e: any) => {
-    const { key } = e;
+  const deleteCharacter = () => {
+    if (text[cursor.row].length - 1 > cursor.col) {
+      const newText = [...text];
+      const currentLine = newText[cursor.row];
+      const firstPartOfLine = [...currentLine.slice(0, cursor.col + 1)];
 
+      firstPartOfLine.pop();
+      newText[cursor.row] = [
+        ...firstPartOfLine,
+        ...currentLine.slice(cursor.col + 1),
+      ];
+
+      setText(newText);
+    } else if (text.length - 1 > cursor.row) {
+
+      const newText = [...text];
+      const afterLine = newText[cursor.row + 1];
+
+      newText[cursor.row].pop()
+      newText[cursor.row] = [...newText[cursor.row], ...afterLine];
+      newText.splice(cursor.row + 1, 1);
+
+      setText(newText);
+    }
+  };
+
+  const handleKeyDown = (e: any) => {
+    e.preventDefault();
+    const { key } = e;
+    console.log(`key:${key}`);
     if (key === "ArrowRight") {
       moveCursorRight();
     } else if (key === "ArrowLeft") {
@@ -85,21 +117,25 @@ export default function TextEditor({}: Props) {
     } else if (key === "Enter") {
       insertNewLine();
     } else if (key === "Backspace") {
+      backSpaceDeleteCharacter();
+    } else if (key === "Delete") {
       deleteCharacter();
+    } else if (key === " ") {
+      insertCharacter(key, "space");
     } else if (key.length === 1) {
       insertCharacter(key);
     }
   };
 
-  console.log(`array:${JSON.stringify(text,null,2)}`)
+  console.log(`array:${JSON.stringify(text, null, 4)}`);
   return (
-    <div className="relative flex flex-col items-center justify-center bg-stone-900 text-amber-600 leading-5 p-4  shadow-xl rounded-lg h-[80vh] w-[80vw] ">
+    <div className="relative py-4 flex flex-col items-center justify-center bg-stone-900 text-muted-foreground leading-5 p-4  shadow-xl rounded-lg h-[80vh] w-[80vw] ">
       <div className="flex  h-[100%] w-[100%] bg-stone-800 overflow-auto ">
         <EditorText
           text={text}
           cursor={cursor}
           onPointerClick={(row, col) => setCursor({ row, col })}
-          onKeyDown={(e)=>handleKeyDown(e)}
+          onKeyDown={(e) => handleKeyDown(e)}
         />
       </div>
       <EditorFooter row={cursor.row} col={cursor.col} />
@@ -124,7 +160,7 @@ type EditorTextType = {
     col: number;
   };
   onPointerClick: (row: number, col: number) => void;
-  onKeyDown: (e:any) => void;
+  onKeyDown: (e: any) => void;
 };
 
 function EditorText({
@@ -133,7 +169,6 @@ function EditorText({
   onPointerClick,
   onKeyDown,
 }: EditorTextType) {
-
   const renderText = () => {
     return text.map((line, rowIndex) => {
       const isPointerLine: boolean = cursor.row === rowIndex;
@@ -142,8 +177,8 @@ function EditorText({
           <div
             key={rowIndex}
             className={cn(
-              "w-full flex py-0 ",
-              isPointerLine && " border-t-2 border-b-2 border-stone-600"
+              "w-full flex  border-t-[1.5px] border-b-[1.5px] border-transparent",
+              isPointerLine && " border-stone-600"
             )}
           >
             <span
@@ -161,12 +196,12 @@ function EditorText({
                 onClick={() => onPointerClick(rowIndex, colIndex)}
               >
                 <span className={cn(" ")}>
-                  {char !== " "?`${char}`:'\u00A0'}
+                  {char === "space" ? "\u00A0" : `${char}`}
                   {cursor.row === rowIndex && cursor.col === colIndex && (
-                    <div className="absolute inset-0  z-[999] px-px bg-white  h-[18px] justify-self-start" />
+                    <div className="absolute inset-0 animate-blink-cursor  z-[999] px-px bg-white  w-0.5 h-5 justify-self-start" />
                   )}
                 </span>
-              </div>  
+              </div>
             ))}
           </div>
         </>
